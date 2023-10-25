69de1d664036fd4fdcafd37723997bd0536412e5
Pathak <kpp918006@tatatechnologies.com>
2023-10-25 17:46:39 +0530

    Push

---
+++ New.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+		System.out.println("Hello, Pratham");
+		System.out.println("Repo SW_BOM");
+		System.out.println("File imported");
+    }
+}
+
